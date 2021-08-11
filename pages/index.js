919b33e4b8125e0506e8a2618ae7cfadbc967448
Justin Jones <justin.jones@lumiglobal.com>
2021-08-11 15:57:20 +0100

    Starting blog

---
+++ pages/index.js
@@ -0,0 +1,57 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+
+
+/* - getStaticProps only runs at build time (except in dev). */ 
+/* - getServerSideProps(context) - slower - will run once on request to gen server side
+                                 and can have params */
+/* Good guidance on what to use when https://nextjs.org/learn/basics/data-fetching/request-time*/
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>This is a bit about me... me is devlopr.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+
+      /* Blog posts */
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+          
+          ))}
+        </ul>
+      </section>
+
+    </Layout>
+  )
+}
